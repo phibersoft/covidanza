10d103dc632c9d63e99702ed4efd02ba2deaf6e1
Adem Uysal <spirnet@hotmail.com>
2021-08-13 22:01:14 +0300

    Possible Fix

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,7 +3,7 @@ import { rootReducer } from "./reducers/rootReducer";
 
 const store = createStore(
   rootReducer,
-  (window as any)?.__REDUX_DEVTOOLS_EXTENSION__()
+  (window as any)?.__REDUX_DEVTOOLS_EXTENSION__?.()
 );
 
 export default store;
